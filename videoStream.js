623bd6be0b99b5d5ec22ddcb0e88bb3f6e6bcaab
Antoine CHOQUET <achoquet@itlink.fr>
2019-09-10 11:11:02 +0200

    clean code

--- videoStream.js
+++ videoStream.js
@@ -1,133 +1,149 @@
-var Mpeg1Muxer, STREAM_MAGIC_BYTES, VideoStream, events, util, ws
+var ws = require('ws');
+var util = require('util');
+var events = require('events');
+var Mpeg1Muxer = require('./mpeg1muxer');
+var STREAM_MAGIC_BYTES = "jsmp"; // Must be 4 bytes
 
-ws = require('ws')
-
-util = require('util')
-
-events = require('events')
-
-Mpeg1Muxer = require('./mpeg1muxer')
-
-STREAM_MAGIC_BYTES = "jsmp" // Must be 4 bytes
-
-VideoStream = function(options) {
-  this.options = options
-  this.name = options.name
-  this.streamUrl = options.streamUrl
-  this.width = options.width
-  this.height = options.height
-  this.wsPort = options.wsPort
-  this.inputStreamStarted = false
-  this.stream = undefined
-  this.startMpeg1Stream()
-  this.pipeStreamToSocketServer()
-  return this
+var VideoStream = function(options) {
+  this.options = options;
+  this.name = options.name;
+  this.streamUrl = options.streamUrl;
+  this.width = options.width;
+  this.height = options.height;
+  this.wsPort = options.wsPort;
+  this.inputStreamStarted = false;
+  this.stream = undefined;
+  this.startMpeg1Stream();
+  this.pipeStreamToSocketServer();
+  return this;
 }
 
-util.inherits(VideoStream, events.EventEmitter)
-
 VideoStream.prototype.stop = function() {
-  this.wsServer.close()
-  this.stream.kill()
-  this.inputStreamStarted = false
-  return this
+  this.wsServer.close();
+  this.stream.kill();
+  this.inputStreamStarted = false;
+  return this;
 }
 
 VideoStream.prototype.startMpeg1Stream = function() {
-  var gettingInputData, gettingOutputData, inputData, outputData
+  
   this.mpeg1Muxer = new Mpeg1Muxer({
     ffmpegOptions: this.options.ffmpegOptions,
     url: this.streamUrl
-  })
-  this.stream = this.mpeg1Muxer.stream
+  });
+  
+  this.stream = this.mpeg1Muxer.stream;
+  
   if (this.inputStreamStarted) {
-    return
+    return;
   }
+
   this.mpeg1Muxer.on('mpeg1data', (data) => {
-    return this.emit('camdata', data)
-  })
-  gettingInputData = false
-  inputData = []
-  gettingOutputData = false
-  outputData = []
+    return this.emit('camdata', data);
+  });
+
+  var gettingInputData = false;
+  var inputData = [];
+  var gettingOutputData = false;
+  var outputData = [];
+
   this.mpeg1Muxer.on('ffmpegStderr', (data) => {
-    var size
-    data = data.toString()
+    var size;
+    data = data.toString();
+
     if (data.indexOf('Input #') !== -1) {
-      gettingInputData = true
+      gettingInputData = true;
     }
+
     if (data.indexOf('Output #') !== -1) {
-      gettingInputData = false
-      gettingOutputData = true
+      gettingInputData = false;
+      gettingOutputData = true;
     }
+
     if (data.indexOf('frame') === 0) {
-      gettingOutputData = false
+      gettingOutputData = false;
     }
+
     if (gettingInputData) {
-      inputData.push(data.toString())
-      size = data.match(/\d+x\d+/)
+
+      inputData.push(data.toString());
+      size = data.match(/\d+x\d+/);
+
       if (size != null) {
-        size = size[0].split('x')
+
+        size = size[0].split('x');
+
         if (this.width == null) {
-          this.width = parseInt(size[0], 10)
+          this.width = parseInt(size[0], 10);
         }
+
         if (this.height == null) {
-          return this.height = parseInt(size[1], 10)
+          return this.height = parseInt(size[1], 10);
         }
       }
     }
-  })
+  });
+
   this.mpeg1Muxer.on('ffmpegStderr', function(data) {
-    return global.process.stderr.write(data)
-  })
+    return global.process.stderr.write(data);
+  });
+
   this.mpeg1Muxer.on('exitWithError', () => {
-    return this.emit('exitWithError')
-  })
-  return this
+    return this.emit('exitWithError');
+  });
+
+  return this;
 }
 
 VideoStream.prototype.pipeStreamToSocketServer = function() {
+  
   this.wsServer = new ws.Server({
     port: this.wsPort
-  })
+  });
+
   this.wsServer.on("connection", (socket, request) => {
-    return this.onSocketConnect(socket, request)
-  })
+    return this.onSocketConnect(socket, request);
+  });
+
   this.wsServer.broadcast = function(data, opts) {
-    var results
-    results = []
+    
+    var results = [];
+
     for (let client of this.clients) {
       if (client.readyState === 1) {
-        results.push(client.send(data, opts))
+        results.push(client.send(data, opts));
       } else {
-        results.push(console.log("Error: Client from remoteAddress " + client.remoteAddress + " not connected."))
+        results.push(console.log("Error: Client from remoteAddress " + client.remoteAddress + " not connected."));
       }
     }
-    return results
+    return results;
   }
+
   return this.on('camdata', (data) => {
-    return this.wsServer.broadcast(data)
-  })
+    return this.wsServer.broadcast(data);
+  });
 }
 
 VideoStream.prototype.onSocketConnect = function(socket, request) {
-  var streamHeader
   // Send magic bytes and video size to the newly connected socket
   // struct { char magic[4]; unsigned short width, height;}
-  streamHeader = new Buffer(8)
-  streamHeader.write(STREAM_MAGIC_BYTES)
-  streamHeader.writeUInt16BE(this.width, 4)
-  streamHeader.writeUInt16BE(this.height, 6)
+  var streamHeader = new Buffer(8);
+  streamHeader.write(STREAM_MAGIC_BYTES);
+  streamHeader.writeUInt16BE(this.width, 4);
+  streamHeader.writeUInt16BE(this.height, 6);
   socket.send(streamHeader, {
     binary: true
-  })
-  console.log(`${this.name}: New WebSocket Connection (` + this.wsServer.clients.size + " total)")
+  });
 
-  socket.remoteAddress = request.connection.remoteAddress
+  console.log(`${this.name}: New WebSocket Connection (` + this.wsServer.clients.size + " total)");
+
+  socket.remoteAddress = request.connection.remoteAddress;
 
   return socket.on("close", (code, message) => {
-    return console.log(`${this.name}: Disconnected WebSocket (` + this.wsServer.clients.size + " total)")
-  })
+    return console.log(`${this.name}: Disconnected WebSocket (` + this.wsServer.clients.size + " total)");
+  });
 }
 
-module.exports = VideoStream
+util.inherits(VideoStream, events.EventEmitter);
+
+module.exports = VideoStream;
